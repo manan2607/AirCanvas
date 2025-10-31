74314ee13ed750ecc195d88179a36c09f24c8c19
Manan Gupta <manangupta.2607@gmail.com>
2025-10-31 17:47:44 +0530

    Refactor functions and update check_rect logic
    Refactor functions by removing unused ones and updating check_rect logic.

--- functions.py
+++ functions.py
@@ -1,69 +1,21 @@
 import cv2
 import numpy as np 
 
-def clean(img,x,y):
-    canvas=np.ones_like(img)*255
-    return canvas
+def check_rect(x,y,z):
 
-def blue(canvas,x,y): 
-    cv2.circle(canvas,(x+10,y+5),8,(255,0,0),cv2.FILLED)
-    return canvas
-
-def red(canvas,x,y):
-    cv2.circle(canvas,(x+10,y+5),8,(0,0,255),cv2.FILLED)
-    return canvas
-
-def blur(img):
-    blur_img=cv2.GaussianBlur(img,(11,11),0)
-    return blur_img
-
-def eraser(canvas,x,y):
-    cv2.circle(canvas,(x+10,y+5),24,(255,255,255),cv2.FILLED)
-    return canvas
-
-def green(canvas,x,y):
-    cv2.circle(canvas,(x+10,y+5),8,(0,255,0),cv2.FILLED)
-    return canvas
-
-def write_file(a):
-    f=open("mode.txt","w+")
-    f.write(a)
-    f.close()
-
-def read_file():
-    f=open("mode.txt","r+")
-    a=f.read()
-    f.close()
-    return a
-
-def check_rect(x,y):
     if((x>20 and x<100) and (y>5 and y<60)):
-        write_file("clean")
+        return "clean"
     if((x>130 and x<210) and (y>5 and y<60)):
-        write_file("eraser")
+        return "eraser"
     if((x>240 and x<320) and (y>5 and y<60)):
-        write_file("green")
+        return "green"
     if((x>350 and x<430) and (y>5 and y<60)):
-        write_file("red")
+        return "red"
     if((x>470 and x<550) and (y>5 and y<60)):
-        write_file("blue")
+        return "blue"
     if((x>590 and x<630) and (y>5 and y<60)):
-        write_file("save")
-    else:
-        pass
-
-def contour_operatio(mask,img):
-    contour,_=cv2.findContours(mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
-    x,y,w,h=0,0,0,0
-    a=(0,0)
-    for cnt in contour:
-        area=cv2.contourArea(cnt)
-        if area>170:
-            peri=cv2.arcLength(cnt,True)
-            approx=cv2.approxPolyDP(cnt,0.02*peri,True)
-            x,y,w,h=cv2.boundingRect(approx)
-            cv2.circle(img,(x+10,y+5),8,(0,255,0),5)
-    return x,y
+        return "save"
+    return z
 
 def contour_operation(mask,img):
     contour,_=cv2.findContours(mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
@@ -78,9 +30,6 @@ def contour_operation(mask,img):
             cv2.circle(img,(int(x),int(y)),int(w/2),(0,255,255),2)
     return x,y
 
-def hsv(blur_img):
-    hsv_img=cv2.cvtColor(blur_img,cv2.COLOR_BGR2HSV)
-    return hsv_img
 
 def mask_generator(lh,ls,lv,uh,us,uv,hsv_img):
     l_value=np.array([73,115,100])
@@ -93,6 +42,7 @@ def mask_generator(lh,ls,lv,uh,us,uv,hsv_img):
     cv2.dilate(mask,kernel,iterations=1)
 
     return mask
+
 
 def draw_rect(img):
     cv2.rectangle(img,(20,5),(100,60),(0,0,0),cv2.FILLED)
@@ -115,6 +65,7 @@ def draw_rect(img):
     
     return img
 
+
 def track():
     lh=cv2.getTrackbarPos("LH","Setter")
     ls=cv2.getTrackbarPos("LS","Setter")
@@ -124,4 +75,3 @@ def track():
     uv=cv2.getTrackbarPos("UV","Setter")
 
     return lh,ls,lv,uh,us,uv
-
